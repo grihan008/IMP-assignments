8bfd78ea4be6fa78233afd1b35e107b95a7af9cf
Unknown <birdingri@gmail.com>
2017-02-21 09:06:15 +0200

    Fixed Flickr http problem

--- Lab6/script.js
+++ Lab6/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#searchform").submit(function(e){
     e.preventDefault();
     $("#images").html("");
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",{
+    $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",{
       tags:$("#search").val(),
       tagmode: "any",
       format: "json"
